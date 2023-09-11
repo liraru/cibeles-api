68deb4985482f6bcc61cc852bdf43cf8f14b44e7
Lorena Sineiro <losingar@gmail.com>
2023-09-11 15:41:52 +0200

    creating relation tables

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -3,18 +3,9 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: ENTITIES.Users })
 export class User {
-  @PrimaryGeneratedColumn()
-  id?: number;
-
-  @Column()
-  name: string;
-
-  @Column()
-  height: number;
-
-  @Column()
-  birthdate: string;
-
-  @Column()
-  gender: string;
+  @PrimaryGeneratedColumn() id?: number;
+  @Column() name: string;
+  @Column() height: number;
+  @Column() birthdate: string;
+  @Column() gender: string;
 }
