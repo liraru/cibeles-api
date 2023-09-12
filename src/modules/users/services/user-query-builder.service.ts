ce290ca2a1731a5b5627d508ec5ecf0a2d83aaba
Lorena Sineiro <losingar@gmail.com>
2023-09-12 08:58:36 +0200

    all user crud query builders

--- src/modules/users/services/user-query-builder.service.ts
+++ src/modules/users/services/user-query-builder.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/modules/users/entities/user.entity';
-import { DataSource, InsertResult } from 'typeorm';
+import { DataSource, InsertResult, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class UserQueryBuilderService {
@@ -14,6 +14,15 @@ export class UserQueryBuilderService {
       .getRawMany();
   }
 
+  getUserById(userId: number): Promise<User> {
+    return this._datasource
+      .getRepository(User)
+      .createQueryBuilder(`u`)
+      .select(`u.id, u.name, u.height, u.birthdate, u.gender`)
+      .where(`u.id = ${userId}`)
+      .getRawOne();
+  }
+
   createUser(user: User): Promise<InsertResult> {
     return this._datasource
       .getRepository(User)
@@ -24,12 +33,28 @@ export class UserQueryBuilderService {
       .execute();
   }
 
-  getUserById(userId: number): Promise<User> {
+  updateUser(user: User): Promise<UpdateResult> {
     return this._datasource
       .getRepository(User)
-      .createQueryBuilder(`u`)
-      .select(`u.id, u.name, u.height, u.birthdate, u.gender`)
-      .where(`u.id = ${userId}`)
-      .getRawOne();
+      .createQueryBuilder()
+      .update(User)
+      .set({
+        name: user.name,
+        height: user.height,
+        birthdate: user.birthdate,
+        gender: user.gender
+      })
+      .where('id = :id', { id: user.id })
+      .execute();
+  }
+
+  changeUserStatus(userId: number, isActive: boolean): Promise<UpdateResult> {
+    return this._datasource
+      .getRepository(User)
+      .createQueryBuilder()
+      .update(User)
+      .set({ isActive: isActive })
+      .where('id = :id', { id: userId })
+      .execute();
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/modules/users/entities/user.entity';
 import { UserQueryBuilderService } from 'src/modules/users/services/user-query-builder.service';
-import { InsertResult } from 'typeorm';
+import { InsertResult, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly _userQB: UserQueryBuilderService) {}
   public getUsers(): Promise<User[]> {
+    console.log(`getUsers`);
     return this._userQB.getUsers();
   }
 
@@ -21,12 +22,12 @@ export class UsersService {
   }
 
   public updateUser(user: User) {
-    console.log(`updateUser -> ${user.name}`);
-    return;
+    console.log(`updateUser -> ${JSON.stringify(user)}`);
+    return this._userQB.updateUser(user);
   }
 
-  public deleteUser(userId: number) {
-    console.log(`deleteUser -> ${userId}`);
-    return;
+  public changeUserStatus(userId: number, isActive: boolean): Promise<UpdateResult> {
+    console.log(`changeUserStatus -> ${userId}`);
+    return this._userQB.changeUserStatus(userId, isActive);
   }
 }
