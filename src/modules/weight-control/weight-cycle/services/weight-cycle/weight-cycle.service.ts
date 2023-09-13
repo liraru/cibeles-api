a4320b89b30d55c32e78e695b3503ef81e90ff94
Lorena Sineiro <losingar@gmail.com>
2023-09-13 09:52:48 +0200

    unvalidated weight cycle service

--- src/modules/weight-control/weight-cycle/services/weight-cycle/weight-cycle.service.ts
+++ src/modules/weight-control/weight-cycle/services/weight-cycle/weight-cycle.service.ts
@@ -1,13 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { WeightCycle } from 'src/modules/weight-control/entities/weight-cycle.entity';
+import { WeightCycleQueryBuilderService } from 'src/modules/weight-control/weight-cycle/services/weight-cycle-query-builder/weight-cycle-query-builder.service';
+import { InsertResult, UpdateResult } from 'typeorm';
 
 @Injectable()
 export class WeightCycleService {
-  /*
-  ➡️ get all cycles
-  ➡️ get cycles by user
-  ➡️ get current user cycle
-  ➡️ add cycle
-  ➡️ edit cycle
-  ➡️ activate/deactivate cycle
-  */
+  constructor(private readonly _qb: WeightCycleQueryBuilderService) {}
+
+  // ➡️ get all cycles
+  getAll(): Promise<WeightCycle[]> {
+    return this._qb.getAll();
+  }
+
+  // ➡️ get cycles by user
+  getAllByUser(userId: number): Promise<WeightCycle[]> {
+    return this._qb.getAllByUser(userId);
+  }
+
+  // ➡️ get current user cycle
+  getCurrentUser(userId: number): Promise<WeightCycle> {
+    return this._qb.getCurrentCycleByUser(userId);
+  }
+
+  // ➡️ add cycle
+  create(cycle: WeightCycle): Promise<InsertResult> {
+    return this._qb.createWeightCycle(cycle);
+  }
+
+  // ➡️ edit cycle
+  update(cycle: WeightCycle): Promise<UpdateResult> {
+    return this._qb.updateWeightCycle(cycle);
+  }
+
+  // ➡️ activate/deactivate cycle
+  changeStatus(isActive: boolean, userId: number): Promise<UpdateResult> {
+    return this._qb.updateWeightCycleStatus(isActive, userId);
+  }
 }
