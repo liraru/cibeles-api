e20a06d005e3128a91d155c9e2c20ff847e1fd3e
Lorena Sineiro <losingar@gmail.com>
2023-09-13 10:00:42 +0200

    wip weight cycle controller

--- src/modules/weight-control/weight-cycle/weight-cycle.controller.ts
+++ src/modules/weight-control/weight-cycle/weight-cycle.controller.ts
@@ -1,21 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { WeightCycle } from 'src/modules/weight-control/entities/weight-cycle.entity';
 import { WeightCycleService } from 'src/modules/weight-control/weight-cycle/services/weight-cycle/weight-cycle.service';
 
 @Controller('weight-cycle')
 export class WeightCycleController {
   constructor(private readonly _weightCycleService: WeightCycleService) {}
 
-  /*
-  ➡️ get all cycles
-  @Get('')
-  getAllCycles(@Query() query): Promise<WeightCycle[]> {
-
+  // ➡️ get all cycles
+  @Get(``)
+  getAll(): Promise<WeightCycle[]> {
+    return this._weightCycleService.getAll();
   }
 
-  ➡️ get cycles by user
-  ➡️ get current user cycle
-  ➡️ add cycle
-  ➡️ edit cycle
-  ➡️ activate/deactivate cycle
-  */
+  // ➡️ get cycles by user
+  @Get(`user/:id`)
+  getAllByUser(@Param('id') userId: number): Promise<WeightCycle[]> {
+    return this._weightCycleService.getAllByUser(userId);
+  }
+
+  // ➡️ get current user cycle
+  // ➡️ add cycle
+  // ➡️ edit cycle
+  // ➡️ activate/deactivate cycle
 }
