5dd247512d89f9ae93d5deb62baa5738b787ff46
Lorena Sineiro <losingar@gmail.com>
2023-09-13 16:00:45 +0200

    weight cycle controller methods unvalidated

--- src/modules/weight-control/weight-cycle/weight-cycle.controller.ts
+++ src/modules/weight-control/weight-cycle/weight-cycle.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { WeightCycle } from 'src/modules/weight-control/entities/weight-cycle.entity';
 import { WeightCycleService } from 'src/modules/weight-control/weight-cycle/services/weight-cycle/weight-cycle.service';
+import { InsertResult, UpdateResult } from 'typeorm';
 
 @Controller('weight-cycle')
 export class WeightCycleController {
@@ -19,7 +20,26 @@ export class WeightCycleController {
   }
 
   // ➡️ get current user cycle
+  @Get(`user-current`)
+  getUserCurrent(userId: number): Promise<WeightCycle> {
+    return this._weightCycleService.getCurrentUser(userId);
+  }
+
   // ➡️ add cycle
+  @Post(`create`)
+  createCycle(cycle: WeightCycle): Promise<InsertResult> {
+    return this._weightCycleService.create(cycle);
+  }
+
   // ➡️ edit cycle
+  @Put(`update`)
+  updateCycle(cycle: WeightCycle): Promise<UpdateResult> {
+    return this._weightCycleService.update(cycle);
+  }
+
   // ➡️ activate/deactivate cycle
+  @Put(`status`)
+  changeStatus(isActive: boolean, userId: number): Promise<UpdateResult> {
+    return this._weightCycleService.changeStatus(isActive, userId);
+  }
 }
